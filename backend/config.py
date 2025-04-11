fabdb48078612983a21b86b6a6341a905c5c75fb
RydCri <107228115+RydCri@users.noreply.github.com>
2025-04-11 13:04:40 -0500

    dev config

---
+++ backend/config.py
@@ -0,0 +1,11 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+    SECRET_KEY = 'your-secret-key'
+    SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(basedir, 'app.db')}"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    UPLOAD_FOLDER = os.path.join(basedir, 'uploads')
+
+# db.init_app(app)
