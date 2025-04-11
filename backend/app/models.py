552471e1baf478f8aff39ad4bb7a78644f8f5a1c
RydCri <107228115+RydCri@users.noreply.github.com>
2025-04-11 13:05:15 -0500

    extensions import

---
+++ backend/app/models.py
@@ -0,0 +1,21 @@
+from .extensions import db
+from werkzeug.security import generate_password_hash, check_password_hash
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    email = db.Column(db.String(150), unique=True, nullable=False)
+    password_hash = db.Column(db.String(200), nullable=False)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+class Flight(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(150), nullable=False)
+    glb_path = db.Column(db.String(200), nullable=False)
+    ndvi_path = db.Column(db.String(200))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
