0ef475e02232cd3a9a2ebd7493ec83f218bb655d
RydCri <107228115+RydCri@users.noreply.github.com>
2025-04-11 16:03:28 -0500

    one to many

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,6 +1,7 @@
 from .extensions import db
 from werkzeug.security import generate_password_hash, check_password_hash
 
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True, nullable=False)
@@ -12,6 +13,10 @@ class User(db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    # One-to-many
+    flights = db.relationship('Flight', backref='user', lazy=True)
+
 
 class Flight(db.Model):
     id = db.Column(db.Integer, primary_key=True)
