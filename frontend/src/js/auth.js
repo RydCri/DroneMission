b1c2ff3568ca57689c663d8dfd633851944a345b
RydCri <107228115+RydCri@users.noreply.github.com>
2025-04-12 18:20:13 -0500

    auth event handles passed to main.js

--- frontend/src/js/auth.js
+++ frontend/src/js/auth.js
@@ -2,8 +2,8 @@ import { ModalManager } from './modal.js';
 
 export function setupAuth() {
     const loginForm = document.getElementById('login-form');
-
-    if (!loginForm) return;
+    // Call this once on page load
+    checkLoginStatus();
 
     loginForm.addEventListener('submit', async (e) => {
         e.preventDefault();
@@ -20,21 +20,51 @@ export function setupAuth() {
         if (res.ok) {
             ModalManager.hide('login-modal');
             document.dispatchEvent(new CustomEvent('user-logged-in'));
+            updateAuthUI(true);
+            console.log(res.body)
         } else {
             alert('Login failed.');
         }
     });
+    async function checkLoginStatus() {
+        const res = await fetch('http://127.0.0.1:5000/auth/session', {
+            method: 'GET',
+            credentials: 'include'
+        });
+        const data = await res.json();
+        const userInfo = document.getElementById('user-info')
+        const userFlights = document.getElementById('user-flights')
+        let user_icon;
+        if(!data.user_icon){
+            user_icon = "../src/user_icon.png"
+        } else{
+            user_icon = data.user_icon;
+        }
+            userFlights.innerText = `${data.username}'s Flights`
+            userInfo.innerHTML = '';
+            userInfo.innerHTML += `<h3 id="username" class="text-lg text-white font-bold">${data.username}</h3>
+      <img class="thumbnail" src="${user_icon}" alt="${data.username}'s icon"/>`;
 
-    function updateAuthUI(isLoggedIn) {
-        document.getElementById('login-button').classList.toggle('hidden', isLoggedIn);
-        document.getElementById('profile-button').classList.toggle('hidden', !isLoggedIn);
+        updateAuthUI(data.loggedIn);
+        if (data.loggedIn) {
+        }
     }
 
-// Example usage:
-    if (sessionStorage.getItem('token')) {
-        updateAuthUI(true);
-    } else {
-        updateAuthUI(false);
+    function updateAuthUI(isLoggedIn) {
+        console.log("UPDATE AUTH")
+        const loginBtn = document.getElementById('login-button');
+        const profileBtn = document.getElementById('profile-button');
+
+        if (isLoggedIn) {
+
+            loginBtn.classList.add('hidden');
+            profileBtn.classList.remove('hidden');
+            console.log("User is logged in login should disappear")
+        } else {
+            loginBtn.classList.remove('hidden');
+            profileBtn.classList.add('hidden');
+            console.log("User is NOT logged in Login should be visible")
+        }
     }
 
 }
