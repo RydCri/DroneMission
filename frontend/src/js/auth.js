7b021b498680a29d2b8d690ec4b261453cb4779d
RydCri <107228115+RydCri@users.noreply.github.com>
2025-04-14 21:44:53 -0500

    setUploadForm imported and called once user is logged in checkLoginStatus() now populates DOM with username and icon is called after @ setupAuth import and again after login and after setupProfile calls loadProfile()

--- frontend/src/js/auth.js
+++ frontend/src/js/auth.js
@@ -1,4 +1,5 @@
 import { ModalManager } from './modal.js';
+import { setupUploadForm } from "./profile.js";
 
 export function setupAuth() {
     const loginForm = document.getElementById('login-form');
@@ -21,50 +22,47 @@ export function setupAuth() {
             ModalManager.hide('login-modal');
             document.dispatchEvent(new CustomEvent('user-logged-in'));
             updateAuthUI(true);
-            console.log(res.body)
+
         } else {
             alert('Login failed.');
         }
     });
-    async function checkLoginStatus() {
-        const res = await fetch('http://127.0.0.1:5000/auth/session', {
-            method: 'GET',
-            credentials: 'include'
-        });
-        const data = await res.json();
-        const userInfo = document.getElementById('user-info')
-        const userFlights = document.getElementById('user-flights')
-        let user_icon;
-        if(!data.user_icon){
-            user_icon = "../src/user_icon.png"
-        } else{
-            user_icon = data.user_icon;
-        }
-            userFlights.innerText = `${data.username}'s Flights`
-            userInfo.innerHTML = '';
-            userInfo.innerHTML += `<h3 id="username" class="text-lg text-white font-bold">${data.username}</h3>
-      <img class="thumbnail" src="${user_icon}" alt="${data.username}'s icon"/>`;
 
-        updateAuthUI(data.loggedIn);
-        if (data.loggedIn) {
-        }
+}
+function updateAuthUI(isLoggedIn) {
+    console.log("UPDATE AUTH")
+    const loginBtn = document.getElementById('login-button');
+    const profileBtn = document.getElementById('profile-button');
+
+    if (isLoggedIn) {
+
+        loginBtn.classList.add('hidden');
+        profileBtn.classList.remove('hidden');
+        setupUploadForm()
+    } else {
+        loginBtn.classList.remove('hidden');
+        profileBtn.classList.add('hidden');
     }
-
-    function updateAuthUI(isLoggedIn) {
-        console.log("UPDATE AUTH")
-        const loginBtn = document.getElementById('login-button');
-        const profileBtn = document.getElementById('profile-button');
-
-        if (isLoggedIn) {
-
-            loginBtn.classList.add('hidden');
-            profileBtn.classList.remove('hidden');
-            console.log("User is logged in login should disappear")
-        } else {
-            loginBtn.classList.remove('hidden');
-            profileBtn.classList.add('hidden');
-            console.log("User is NOT logged in Login should be visible")
-        }
+}
+export async function checkLoginStatus() {
+    console.log('Checking Login.')
+    const userFlights = document.getElementById('user-flights')
+    if(!userFlights) return;
+    const res = await fetch('http://127.0.0.1:5000/auth/session', {
+        method: 'GET',
+        credentials: 'include'
+    });
+    const data = await res.json();
+    console.log(data)
+    const userInfo = document.getElementById('user-info')
+    let user_icon;
+    if (!data.user_icon) {
+        user_icon = "../src/user_icon.png"
+    } else {
+        user_icon = data.user_icon;
     }
-
+    userFlights.innerText = `${data.username}'s Flights`
+    userInfo.innerHTML = '';
+    userInfo.innerHTML += `<h3 id="username" class="text-lg text-white font-bold">${data.username}</h3><img class="thumbnail" src="${user_icon}" alt="${data.username}'s icon"/>`;
+    return userFlights;
 }
