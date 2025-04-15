ad7f7a7ca5f7c6c52a634b575ed9d8502657ad95
RydCri <107228115+RydCri@users.noreply.github.com>
2025-04-15 15:25:16 -0500

    files can be nullable ( doesn't accept in the post route atm )

--- backend/app/models.py
+++ backend/app/models.py
@@ -21,6 +21,6 @@ class User(db.Model):
 class Flight(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(150), nullable=False)
-    glb_path = db.Column(db.String(200), nullable=False)
-    ndvi_path = db.Column(db.String(200))
+    glb_path = db.Column(db.String(200), nullable=True)
+    ndvi_path = db.Column(db.String(200), nullable=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
