02792cc4a1ea9c4b2879f17400971c1c7991766a
RydCri <107228115+RydCri@users.noreply.github.com>
2025-04-17 02:33:18 -0500

    register pins blueprint

--- backend/app/routes/__init__.py
+++ backend/app/routes/__init__.py
@@ -1,3 +1,4 @@
+from .pins import pins
 from .main import main
 from .auth import auth
 
@@ -5,3 +6,4 @@ from .auth import auth
 def register_routes(app):
     app.register_blueprint(main)
     app.register_blueprint(auth)
+    app.register_blueprint(pins)
